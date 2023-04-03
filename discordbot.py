4fc9c92e0858faa939e5f56ee15ad5f9ff12d4d1
KION09 <91176140+KION09@users.noreply.github.com>
2023-04-03 18:05:21 +0900

    Initial commit

---
+++ discordbot.py
@@ -0,0 +1,32 @@
+from cmath import log
+from distutils.sysconfig import PREFIX
+import discord
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+PREFIX = os.environ['PREFIX']
+TOKEN = os.environ['TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == f'{PREFIX}call':
+        await message.channel.send("callback!")
+
+    if message.content.startswith(f'{PREFIX}hello'):
+        await message.channel.send('Hello!')
+
+
+try:
+    client.run(TOKEN)
+except discord.errors.LoginFailure as e:
+    print("Improper token has been passed.")
